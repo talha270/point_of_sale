1f422c9ff3f44f55f42850a612227ecee991f1eb
talha270 <144779558+talha270@users.noreply.github.com>
2025-03-06 13:14:46 +0500

    commit

--- pos/macos/Flutter/GeneratedPluginRegistrant.swift
+++ pos/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -11,6 +11,7 @@ import path_provider_foundation
 import printing
 import shared_preferences_foundation
 import sqflite
+import sqlite3_flutter_libs
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
@@ -19,4 +20,5 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   PrintingPlugin.register(with: registry.registrar(forPlugin: "PrintingPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
+  Sqlite3FlutterLibsPlugin.register(with: registry.registrar(forPlugin: "Sqlite3FlutterLibsPlugin"))
 }
